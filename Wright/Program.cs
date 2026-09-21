673a3eb144e68de3f82cae85500263dc1d8c962d
Ahmed Mustafa <am1804067@gmail.com>
2026-09-21 20:03:25 +0300

    Connect database via connection string + create shared interfaces for db entities

--- Wright/Program.cs
+++ Wright/Program.cs
@@ -1,8 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using Wright.Contexts;
+using Wright.Shared.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+
+var modules = typeof(Program).Assembly.GetTypes()
+	.Where(t => typeof(IModule).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract)
+	.Select(t => (IModule)Activator.CreateInstance(t)!);
+
+foreach (var module in modules)
+{
+    module.Register(builder.Services, builder.Configuration, builder.Environment);
+}
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+	{
+		builder.AllowAnyOrigin()
+			   .AllowAnyMethod()
+			   .AllowAnyHeader();
+	});
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
+
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
@@ -19,5 +51,12 @@ app.MapGet("/health", () =>
     return "Wright is healthy";
 })
 .WithName("HealthCheck");
+
+foreach (var module in modules)
+{
+	module.Configure(app);
+}
+
+app.MapControllers();
 
 app.Run();
